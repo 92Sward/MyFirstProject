784fc4816e79361d0e58c3cb6b8f7f93f61fe8e1
Carles Cano Casablanca <92sward@gmail.com>
2016-12-01 21:23:07 +0100

    First Commit

---
+++ First Script.R
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+subject   <- "data driven security"
+language  <- "R"
+print(paste("This is a hands on project written in",
+            language,
+            "for ",
+            subject,
+            "subject",
+            sep = " "))
